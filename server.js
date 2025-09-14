f517b2552905bb2b1af35c0dbf59254c3bcba570
dnsTechnology <dns.technology.com.np@gmail.com>
2025-09-14 12:28:37 +0545

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND || "http://localhost:5173",
+    origin: "https://dns-sigma.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
