3ac1bb80ce9a3452c9784f3916e5aa90ba2e0fc6
SaradBhatta12 <saradbhatt2@gmail.com>
2025-09-14 11:53:19 +0545

    adding frontend url in backend server for cors config

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
